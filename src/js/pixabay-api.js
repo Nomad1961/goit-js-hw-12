b2bd069ab0dfa3bf3fef0920b5dad89b08d8e489
Seanomad61 <161769498+Nomad1961@users.noreply.github.com>
2024-05-26 14:24:44 +0300

    Summary

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,3 @@
-// pixabay-api.js
-// const apiKey = '43839854-7e39202c3c35776610ceb4193';
-
-// export async function fetchImages(searchTerm) {
-//   const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`;
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   return data.hits;
-// }
-// pixabay-api.js
 const apiKey = '43839854-7e39202c3c35776610ceb4193';
 
 export async function fetchImages(searchTerm, currentPage) {
